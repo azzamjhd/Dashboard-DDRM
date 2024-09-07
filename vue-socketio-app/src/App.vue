ed8a701f02922295f16bf23d07a824ccae4634bb
azzamjhd <71210715+azzamjhd@users.noreply.github.com>
2024-09-07 20:34:35 +0700

    remove bootstrapvue3

--- vue-socketio-app/src/App.vue
+++ vue-socketio-app/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="app" class="container mt-5 text-center">
-    <h1 class=" mb-5">Vue.js Flask-SocketIO</h1>
-    <b-input-group :prepend="username" class="mt-3 mb-3 md" size="md">
-      <b-icon icon="person-fill"></b-icon>
-      <b-form-input v-model="message" placeholder="Enter a message"></b-form-input>
-      <b-input-group-append>
-        <b-button variant="outline-primary" @click="sendMessage">Send message</b-button>
-      </b-input-group-append>
-    </b-input-group>
-    <b-form-textarea v-model="response" rows="5" readonly></b-form-textarea>
+  <div id="app" class="container mt-5">
+    <h1 class="mb-5 text-center">Chat Example</h1>
+    <div class="input-group my-3 shadow-sm">
+      <span class="input-group-text text-bg-success" id="message-prefix">Username: {{ username }}</span>
+      <input type="text" class="form-control" v-model="message" placeholder="Enter a message" aria-label="Message" aria-describedby="message-prefix">
+      <button class="btn btn-primary" @click="sendMessage">Send message</button>
+    </div>
+    <!-- <textarea class="form-control overflow-hidden shadow" v-model="response" rows="5" readonly></textarea> -->
+    
+    <div class="h-50 overflow-hidden">
+      <h2 class="text-center">Messages</h2>
+      <div class="list-group list-group-flush" id="messages">
+        <!-- Received Messages goes here -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,8 +32,22 @@ export default {
     this.socket = io('http://localhost:5000');
     this.username = Math.random().toString(36).substring(7);
 
+    const messageList = document.getElementById('messages');
+
     this.socket.on('message', (data) => {
       this.response = data.username + ': ' + data.message + '\n' + this.response;
+      const messageItem = document.createElement('div');
+      messageItem.classList.add('list-group-item');
+      messageItem.innerHTML = data.username == this.username ? `
+        <div class="list-group list-group-horizontal">
+          <span class="list-group-item text-bg-success" >${data.username}</span>
+          <span class="list-group-item flex-fill border-0">${data.message}</span>
+        </div> ` : `
+        <div class="list-group list-group-horizontal">
+          <span class="list-group-item text-bg-primary" >${data.username}</span>
+          <span class="list-group-item flex-fill border-0">${data.message}</span>
+        </div>`;
+      messageList.insertBefore(messageItem, messageList.firstChild);
     });
     this.socket.on('connect', () => {
       console.log('Connected to the server: ', this.socket.id);
